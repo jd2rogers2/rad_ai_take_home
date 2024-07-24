af98df586f4c433d320a79654eb4613ebcb6434f
james rogers <jd2rogers2@gmail.com>
2024-07-24 16:46:50 -0700

    get requests working, hot reloading

--- api/main.py
+++ api/main.py
@@ -1,6 +1,7 @@
-from typing import Union
-
 from fastapi import FastAPI
+import requests
+from pydantic import BaseModel
+from typing import Union
 
 app = FastAPI()
 
@@ -9,7 +10,52 @@ app = FastAPI()
 def read_root():
     return {"Hello": "World"}
 
+class Location(BaseModel):
+    latitude: float
+    longitude: float
+    human_address: str
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+class FoodTruckPermit(BaseModel):
+    objectid: str
+    applicant: str
+    facilitytype: str
+    cnn: str
+    locationdescription: str
+    address: str
+    blocklot: str
+    block: str
+    lot: str
+    permit: str
+    status: str # "APPROVED" | "REQUESTED" | "EXPIRED" | "SUSPENDED"
+    fooditems: str # description
+    x: float
+    y: float
+    latitude: float
+    longitude: float
+    schedule: str # url
+    approved: str # datetime
+    received: str # date
+    priorpermit: int
+    expirationdate: str # datetime
+    location: Location
+    dayshours: Union[str, None] = None
+    # ":@computed_region_yftq_j783": "10",
+    # ":@computed_region_p5aj_wyqh": "3",
+    # ":@computed_region_rxqg_mtj9": "8",
+    # ":@computed_region_bh8s_q3mv": "58",
+    # ":@computed_region_fyvs_ahh9": "1"
+
+@app.get("/search")
+def search(
+    status: str = "APPROVED",
+    applicant: Union[str, None] = None,
+    street: Union[str, None] = None,
+):
+    query = f"status={status}"
+    if applicant:
+        query += f"&applicant={applicant}"
+    if street:
+        query += f"&street={street}"
+    res = requests.get(f"https://data.sfgov.org/resource/rqzj-sfat.json?{query}")
+    print(len(res.json()))
+    return res.json()
